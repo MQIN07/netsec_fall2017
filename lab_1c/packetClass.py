20801a8786091d4c18bd90132ed4f4b549112490
MengqiQ <31710054+MengqiQ@users.noreply.github.com>
2017-09-13 22:50:52 -0400

    Add files via upload

---
+++ lab_1c/packetClass.py
@@ -0,0 +1,41 @@
+from playground.network.packet import PacketType
+from playground.network.packet.fieldtypes import UINT32, STRING
+
+class RequestRecommandation(PacketType):
+    DEFINITION_IDENTIFIER = "lab2b.student_m.RequuestRecommandation"
+    DEFINITION_VERSION = "1.0"
+
+
+class Question(PacketType):
+    DEFINITION_IDENTIFIER = "lab2b.student_m.Question"
+    DEFINITION_VERSION = "1.0"
+
+    FIELDS = [
+                ("ID", UINT32),
+                ("ask", STRING),
+    ]
+
+class Answer(PacketType):
+    DEFINITION_IDENTIFIER = "lab2b.student_m.Answer"
+    DEFINITION_VERSION = "1.0"
+
+    FIELDS = [
+                ("ID", UINT32),
+                ("answer", STRING),
+    ]
+
+class Result(PacketType):
+    DEFINITION_IDENTIFIER = "lab2b.student_m.Result"
+    DEFINITION_VERSION = "1.0"
+
+    FIELDS = [
+                ("ID", UINT32),
+                ("product", STRING),
+    ]
+
+
+
+
+#pktBytes = packet1.__serialize__()+ packet2.__serialize__() + packet3.__serialize__() + packet4.__serialize__()
+#packetTest = PacketType.Deserialize(pktBytes)
+#if packet1 == packetTest:
